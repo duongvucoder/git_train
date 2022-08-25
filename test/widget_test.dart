891e6e067173a55317446ca12b428d77080713f2
duongvucoder <111757898+duongvucoder@users.noreply.github.com>
2022-08-26 00:17:33 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_chat_app/main.dart';
+import 'package:flutter_chat_app/main.dart';
 
 import 'package:flutter_chat_app/main.dart';
 
